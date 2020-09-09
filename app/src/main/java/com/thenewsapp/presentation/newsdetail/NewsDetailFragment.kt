ef28250818aa7cc6505a70cac4e0f46412100d91
lucasnobile <12Archa12!>
2020-09-09 16:24:49 -0300

    Change onActivityCreated for onViewCreated.

--- app/src/main/java/com/thenewsapp/presentation/newsdetail/NewsDetailFragment.kt
+++ app/src/main/java/com/thenewsapp/presentation/newsdetail/NewsDetailFragment.kt
@@ -37,8 +37,8 @@ class NewsDetailFragment : Fragment() {
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         getSelectedNews()
     }
