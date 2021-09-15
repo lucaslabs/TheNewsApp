68eada5c9f3793d04616da6f4669443bccdd0e58
lucasnobile <12Archa12!>
2021-09-15 14:43:20 -0300

    Add everything needed to start using Compose :)

---
+++ app/src/main/java/com/thenewsapp/presentation/compose/ComposeActivity.kt
@@ -0,0 +1,15 @@
+package com.thenewsapp.presentation.compose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+
+class ComposeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { 
+            Text(text = "Hello Compose World!")
+        }
+    }
+}
