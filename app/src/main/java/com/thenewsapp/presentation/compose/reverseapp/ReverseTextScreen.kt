b108c78f5324ee86606b2aeed3b8075b565a257b
Lucas Nobile <lucas.nobile@strike.me>
2022-09-07 12:23:17 -0300

    Show user input in a list

--- app/src/main/java/com/thenewsapp/presentation/compose/reverseapp/ReverseTextScreen.kt
+++ app/src/main/java/com/thenewsapp/presentation/compose/reverseapp/ReverseTextScreen.kt
@@ -4,51 +4,97 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.thenewsapp.R
 import com.thenewsapp.presentation.compose.theme.AppTheme
+import com.thenewsapp.presentation.compose.theme.Large
+import com.thenewsapp.presentation.compose.theme.Medium
+import com.thenewsapp.presentation.compose.theme.Small
 
 @Composable
 fun ReverseTextScreen(viewModel: ReverseTextViewModel = ReverseTextViewModel()) {
     Column(modifier = Modifier.fillMaxSize()) {
-        var inputText by remember { mutableStateOf("") }
+        val reversedTextState: State<String> = viewModel.reversedText.observeAsState("")
 
-        val reversedTextState = viewModel.reversedText.observeAsState()
+        val inputTextListState: State<List<String>> =
+            viewModel.inputTextList.observeAsState(initial = emptyList())
 
-        reversedTextState.value?.let { text ->
-            Text(
-                text = text,
-                modifier = Modifier
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-                    .fillMaxWidth()
-            )
-        }
+        val paddingModifier = Modifier
+            .padding(horizontal = Large, vertical = Medium)
+            .fillMaxWidth()
 
-        TextField(
-            value = inputText,
-            onValueChange = { inputText = it },
-            label = { Text(text = stringResource(id = R.string.enter_text)) },
-            modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 8.dp)
-                .fillMaxWidth()
+        ReversedTextSection(
+            text = reversedTextState.value,
+            onTextValueChange = { text -> viewModel.onTextValueChange(text) },
+            modifier = paddingModifier
         )
 
-        Button(
-            onClick = { viewModel.onTextValueChange(inputText) },
-            modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 8.dp)
-                .fillMaxWidth()) {
-            Text(text = stringResource(id = R.string.reverse_text))
+        InputTextListSection(
+            textList = inputTextListState.value,
+            modifier = paddingModifier
+        )
+    }
+}
+
+@Composable
+fun ReversedTextSection(
+    text: String,
+    onTextValueChange: (String) -> Unit,
+    modifier: Modifier,
+) {
+    var inputTextState: String by remember { mutableStateOf(value = "") }
+
+    Text(
+        text = stringResource(id = R.string.reversed_text, text),
+        fontWeight = FontWeight.Bold,
+        modifier = modifier
+    )
+
+    TextField(
+        value = inputTextState,
+        onValueChange = { inputTextState = it },
+        label = { Text(text = stringResource(id = R.string.enter_text)) },
+        modifier = modifier
+    )
+
+    Button(
+        onClick = { onTextValueChange(inputTextState) },
+        modifier = modifier
+    ) {
+        Text(text = stringResource(id = R.string.reverse_text))
+    }
+}
+
+@Composable
+fun InputTextListSection(textList: List<String>, modifier: Modifier) {
+    Text(
+        text = stringResource(id = R.string.original_input_list),
+        modifier = modifier
+    )
+
+    LazyColumn(modifier = modifier) {
+        items(textList) { text ->
+            InputTextItem(text = text)
         }
     }
+}
+
+@Composable
+fun InputTextItem(text: String) {
+    Text(text = text, Modifier.padding(vertical = Small))
+    Divider(color = Color.Black)
 }
 
 @Preview(
--- app/src/main/java/com/thenewsapp/presentation/compose/reverseapp/ReverseTextViewModel.kt
+++ app/src/main/java/com/thenewsapp/presentation/compose/reverseapp/ReverseTextViewModel.kt
@@ -6,10 +6,18 @@ import androidx.lifecycle.ViewModel
 
 class ReverseTextViewModel : ViewModel() {
 
-    private val _reversedText = MutableLiveData("")
+    private val _reversedText = MutableLiveData<String>()
     val reversedText: LiveData<String> = _reversedText
 
+    private val _inputTextList = MutableLiveData<List<String>>()
+    val inputTextList: LiveData<List<String>> = _inputTextList
+
+    private val textList = mutableListOf<String>()
+
     fun onTextValueChange(text: String) {
+        textList.add(text)
+        _inputTextList.value = textList
+
         _reversedText.value = text.reversed()
     }
 }
