53590d49de18f3952426f567551556c090b841b7
Lucas Nobile <lucas.nobile@smallplanet.com>
2023-06-26 15:21:33 -0300

    Add documentation Add dimen file

---
+++ app/src/main/java/com/thenewsapp/presentation/theme/Dimen.kt
@@ -0,0 +1,9 @@
+package com.thenewsapp.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+val dimenXXLarge = 64.dp
+val dimenXLarge = 32.dp
+val dimenLarge = 16.dp
+val dimenMedium = 8.dp
+val dimenSmall = 4.dp
